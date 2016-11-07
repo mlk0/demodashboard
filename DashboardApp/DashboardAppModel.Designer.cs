c0b0793aa662203944f887c8b49fd18365c45ac3
Mirko Atanasov <mirko.atanasov@gmail.com>
2016-11-07 04:37:20 -0500

    updated the .net framework

--- DashboardApp/DashboardAppModel.Designer.cs
+++ DashboardApp/DashboardAppModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Projects\DashboardApp\DashboardApp\DashboardAppModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GiTs\modernizing-websites-microsoft-azure\demos\DashboardApp\DashboardAppModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
