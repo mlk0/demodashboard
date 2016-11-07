4a3ea7f8905e18e13e8bff009ac09fe4062cb771
Mirko Atanasov <mirko.atanasov@gmail.com>
2016-11-07 05:13:13 -0500

    removing the database dependency on initial load and emulation of the dahsboard report instance

--- DashboardApp/Controllers/HomeController.cs
+++ DashboardApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using DashboardApp.Models;
+using Newtonsoft.Json;
 
 namespace DashboardApp.Controllers
 {
@@ -9,13 +10,19 @@ namespace DashboardApp.Controllers
     public ActionResult Index()
     {
       var db = new DashboardAppEntities();
-      var dashboardReport = new DashboardReport
-      {
-        NewComments = db.Comments.Count(),
-        NewTasks = db.Tasks.Count(),
-        NewOrders = db.Orders.Count(),
-        SupportTickets = db.SupportTickets.Count()
-      };
+
+
+
+      //var dashboardReport = new DashboardReport
+      //{
+      //  NewComments = db.Comments.Count(),
+      //  NewTasks = db.Tasks.Count(),
+      //  NewOrders = db.Orders.Count(),
+      //  SupportTickets = db.SupportTickets.Count()
+      //};
+
+            var dashboardReportJsonString = @"{'NewComments':87,'NewTasks':27,'NewOrders':781,'SupportTickets':54}";
+            var dashboardReport = JsonConvert.DeserializeObject<DashboardReport>(dashboardReportJsonString);
 
       ViewBag.Title = "Home";
       return View(dashboardReport);
